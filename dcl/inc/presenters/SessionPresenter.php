830877e7178983cb3b16c692136073e4a5e37397
Michael Dean <mdean5@kc.rr.com>
2014-07-18 15:23:37 -0500

    Remove session ID from sessions list so it isn't disclosed

--- dcl/inc/presenters/SessionPresenter.php
+++ dcl/inc/presenters/SessionPresenter.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * This file is part of Double Choco Latte.
- * Copyright (C) 1999-2011 Free Software Foundation
+ * Copyright (C) 1999-2014 Free Software Foundation
  *
  * Double Choco Latte is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -24,20 +24,17 @@ class SessionPresenter
 {
 	public function Index()
 	{
-		global $g_oSec, $g_oSession;
-		
 		commonHeader();
 		
 		$queryHelper = new SessionSqlQueryHelper();
 		$queryHelper->title = 'DCL Sessions';
-		$queryHelper->AddDef('columns', '', array('personnel.short', 'create_date', 'update_date', 'dcl_session_id'));
+		$queryHelper->AddDef('columns', '', array('personnel.short', 'create_date', 'update_date'));
 		$queryHelper->AddDef('order', '', array('personnel.short', 'create_date'));
 
 		$queryHelper->AddDef('columnhdrs', '', array(
 				'User',
 				'Login Date',
-				'Last Access',
-				'Session ID'));
+				'Last Access'));
 
 		$model = new SessionModel();
 		if ($model->query($queryHelper->GetSQL()) == -1)
@@ -50,23 +47,9 @@ class SessionPresenter
 		$tableHelper->addColumn('User', 'string');
 		$tableHelper->addColumn('Login Date', 'string');
 		$tableHelper->addColumn('Last Access', 'string');
-		$tableHelper->addColumn('Session ID', 'string');
-		
+
 		$tableHelper->addToolbar(menuLink('', 'menuAction=Session.Index'), 'Refresh');
 
-		if (count($allRecs) > 0 && $g_oSec->HasPerm(DCL_ENTITY_GLOBAL, DCL_ENTITY_ADMIN))
-		{
-			$tableHelper->addColumn(STR_CMMN_OPTIONS, 'html');
-			for ($i = 0; $i < count($allRecs); $i++)
-			{
-				$options = '';
-				if ($allRecs[$i][3] != $g_oSession->dcl_session_id)
-					$options = '<a href="' . menuLink('', 'menuAction=Session.Kill&session_id=' . $allRecs[$i][3]) . '">' . 'Kill' . '</a>';
-
-				$allRecs[$i][] = $options;
-			}
-		}
-		
 		$tableHelper->setData($allRecs);
 		$tableHelper->setShowRownum(true);
 		$tableHelper->render();
